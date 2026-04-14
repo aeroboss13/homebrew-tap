11e611e9181a83dcb6f47598f69a0a56dc225162
root <root@p.bardin2017.fvds.ru>
2026-04-14 12:15:57 +0300

    Fix pabit-desktop cask release URL.
    Pin download URL to desktop-v1 tag instead of latest to avoid 404.

--- Casks/pabit-desktop.rb
+++ Casks/pabit-desktop.rb
@@ -1,8 +1,8 @@
 cask "pabit-desktop" do
-  version :latest
+  version "desktop-v1"
   sha256 :no_check
 
-  url "https://github.com/aeroboss13/pabit_prod/releases/latest/download/PabitDesktop.zip"
+  url "https://github.com/aeroboss13/pabit_prod/releases/download/desktop-v1/PabitDesktop.zip"
   name "Pabit Desktop"
   desc "Online macOS desktop wrapper for Pabit"
   homepage "https://github.com/aeroboss13/pabit_prod"

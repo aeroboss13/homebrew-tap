fc9860f784f37946a0421f8dd6cc4e8b5ce47f01
root <root@p.bardin2017.fvds.ru>
2026-04-14 12:18:11 +0300

    Use public releases repository for cask binary URL.
    Point pabit-desktop cask to public pabit-desktop-releases assets.

--- Casks/pabit-desktop.rb
+++ Casks/pabit-desktop.rb
@@ -2,10 +2,10 @@ cask "pabit-desktop" do
   version "desktop-v1"
   sha256 :no_check
 
-  url "https://github.com/aeroboss13/pabit_prod/releases/download/desktop-v1/PabitDesktop.zip"
+  url "https://github.com/aeroboss13/pabit-desktop-releases/releases/download/desktop-v1/PabitDesktop.zip"
   name "Pabit Desktop"
   desc "Online macOS desktop wrapper for Pabit"
-  homepage "https://github.com/aeroboss13/pabit_prod"
+  homepage "https://github.com/aeroboss13/pabit-desktop-releases"
 
   depends_on macos: ">= :ventura"
 
